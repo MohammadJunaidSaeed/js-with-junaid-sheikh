35ec7f45a4fcc866a330d9b9984649fc0f987159
MohammadJunaidSaeed <38969405+MohammadJunaidSaeed@users.noreply.github.com>
2024-05-16 10:43:36 +0000

    modified

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
 console.log("Hello Junaid Sheikh");
+let accountId = 1234;
+let accountEmail = "junaid@gmail.com";
+let accountPassword = 122222;
+let accountStatus = true;
+console.table(typeof[accountId, accountEmail,accountPassword, accountStatus]);
